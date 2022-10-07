5fe855ae8216a839f03fbcc4e7a43ff0501a3fce
Sawyer Cutler <sawyer.cutler@gmail.com>
2022-10-07 13:51:17 -0700

    Core Pages Added with Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
 import './App.css';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Pages from './pages';
 
 function App() {
   return (
     <main>
-      <p>Hello World</p>
+      <Router>
+        <Routes>
+          <Route path='/' element={ <Pages.LandingPage />} />
+          <Route path='/station' element={ <Pages.StationPage isDeveloper={false} /> } />
+          <Route path='/developers' element={ <Pages.StationPage isDeveloper={true} /> } />
+        </Routes>
+      </Router>
     </main>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,11 @@
-export { WalletButton }  from './WalletButton';
+import Layout from './Layout';
+import Navigation from './Navigation';
+import WalletButton from './WalletButton';
+
+const Components = {
+  Layout,
+  Navigation,
+  WalletButton
+}
+
+export default Components;
